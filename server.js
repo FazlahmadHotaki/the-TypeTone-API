c848c49e7cdd3509ef6c37510450a61a666c0a27
Fazlahmad <fazlahmadhotaki7@gmail.com>
2026-09-14 21:44:55 +0430

    fsakdjlfs

--- server.js
+++ server.js
@@ -133,3 +133,4 @@ app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
   console.log(`   Total lessons loaded from lessons.json: ${lessons.length}`);
 });
+// fsda
