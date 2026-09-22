eff616b238dfdaba501e381b61dac5edcfaf5323
Fazlahmad <fazlahmadhotaki7@gmail.com>
2026-09-22 22:01:35 +0430

    clear

--- server.js
+++ server.js
@@ -132,5 +132,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
   console.log(`   Total lessons loaded from lessons.json: ${lessons.length}`);
-  //ahmadi 123456
+  
 });
